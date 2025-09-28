db899c4f42d96dcf7c9d96f20703abb4394193d9
Rahul Sharma <rsharma3639@gmail.com>
2025-09-28 23:12:54 +0530

    linting

--- Myapp/app.py
+++ Myapp/app.py
@@ -7,5 +7,5 @@ def home():
     return f"Hello, CI_CD from Flask ({os.getenv('ENV', 'dev')}) "
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 5000))
-    app.run(host="0.0.0.0", port=5000)   
+    port = int(os.getenv('PORT', '5000'))
+    app.run(host="0.0.0.0", port=port)   
